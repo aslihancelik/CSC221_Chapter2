5c4b7920bcc72b73d47147a0c8b146405b5e7cce
aslihancelik <aslihancelik27@gmail.com>
2024-09-26 17:44:44 -0400

    Added commentsa and cout statements.

--- CSC221_Chapter2.cpp
+++ CSC221_Chapter2.cpp
@@ -1,10 +1,12 @@
 // CSC221_Chapter2.cpp : This file contains the 'main' function. This is a program that calculates 
-// the number of miles per gallon the car gets. Program displays the result.
+// the number of miles per gallon the car gets. Program displays the values of current gallons,
+// current miles that can be traveled and the calculated result of miles per gallon.
 //
 //
 // Course: CSC 221
 // Programmer: Aslihan Celik
 // Date: 09/26/2024
+// Assignment: Chapter 2 Exercise 10 
 
 #include <iostream>
 using namespace std;
@@ -20,6 +22,10 @@ int main()
 
     //Calculates the number of miles per gallon the car gets
 	miles_per_gallon = CURRENT_MILES_TRAVEL / CURRENT_GAS;
+
+	//Display the values used in calculation.
+	cout << "The current number of gallons is " << CURRENT_GAS << "." << endl;
+	cout << "The current miles that can be traveled is " << CURRENT_MILES_TRAVEL << "." << endl;
 
 	//Output the result 
 	cout << "The number of miles per gallon the car gets is " << miles_per_gallon << "." << endl;
